a71eb26bd5a92f6fd5e0be7c07bc8d8bb51d9aec
Moonblye <lars.van.dommelen@xs4all.nl>
2022-09-28 12:15:06 +0200

    SoundSlider gefixt

--- Global_settings.gd
+++ Global_settings.gd
@@ -1,13 +1,13 @@
 extends Node
 
 func Set_MaV_vollume(value):
-	AudioServer.set_bus_volume_db(0, value)
+	AudioServer.set_bus_volume_db(0, value -100)
 
 func Set_music_vollume(value):
-	AudioServer.set_bus_volume_db(1, value)
+	AudioServer.set_bus_volume_db(1, value -100)
 
 func Set_SFX_vollume(value):
-	AudioServer.set_bus_volume_db(2, value)
+	AudioServer.set_bus_volume_db(2, value -100)
 
 func Toggle_fullscreen(value):
 	OS.window_fullscreen = value
