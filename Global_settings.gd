1dddd06eb952f92b0407fb93cf65ef87f485f03a
kaan177 <66557991+kaan177@users.noreply.github.com>
2022-10-28 15:09:47 +0200

    fixed sound sliders

--- Global_settings.gd
+++ Global_settings.gd
@@ -1,13 +1,22 @@
 extends Node
 
 func Set_MaV_vollume(value):
-	AudioServer.set_bus_volume_db(0, value -100)
+	if value == 0:
+		AudioServer.set_bus_mute(0, true)
+	AudioServer.set_bus_volume_db(0, value -30)
 
 func Set_music_vollume(value):
-	AudioServer.set_bus_volume_db(1, value -100)
+	if value == 0:
+		AudioServer.set_bus_mute(1, true)
+	AudioServer.set_bus_volume_db(1, value -30)
 
 func Set_SFX_vollume(value):
-	AudioServer.set_bus_volume_db(2, value -100)
+	if value == 0:
+		AudioServer.set_bus_mute(2, true)
+	AudioServer.set_bus_volume_db(2, value -30)
+
+
+
 
 func Toggle_fullscreen(value):
 	OS.window_fullscreen = value
