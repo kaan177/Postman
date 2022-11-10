00bff34d36ec380f965623b2166f312a0590c15f
kaan177 <knpg2005@gmail.com>
2022-11-10 12:53:35 +0100

    added the working portals

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -1,4 +1,6 @@
 extends Area2D
 
 func _on_Portal_body_entered(body):
+	GlobalScript.From_level = get_parent().name
 	get_tree().change_scene("res://Levels" + self.name + ".tscn")
+	
--- Testing/TestWorld.gd
+++ Testing/TestWorld.gd
@@ -8,6 +8,8 @@ onready var Pause_menu = $PauseMenu
 func _ready():
 	#connect signal met de speler node
 	Player.connect("letter_request", self, "Spawn_letter")
+	if GlobalScript.From_level != null:
+		Player.set_position(get_node(GlobalScript.From_level + "pos").position)
 
 #Spawnt de letter
 func Spawn_letter(Lettervector):
