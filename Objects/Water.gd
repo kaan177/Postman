15b0b2e186223db189181bc008f1b84fb8bc3bbf
kaan177 <knpg2005@gmail.com>
2022-11-14 12:28:44 +0100

    water glijd nu over de vloer

--- Objects/Water.gd
+++ Objects/Water.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 const GRAVITY = Vector2(0, 0.2)
+const UP = Vector2.UP
 
-var Vector = Vector2.ZERO
+var Vector = Vector2.RIGHT
 var Rng = RandomNumberGenerator.new()
 var Move = false
 
@@ -25,26 +26,32 @@ func _physics_process(delta):
 	if Move:
 		Vector += GRAVITY
 		Vector = Vector.normalized()
-
 		var dir = rad2deg(Vector.angle())
-
-
+		
+		
 		var Directions = [Vector2(-1, 0), Vector2(0, -1), Vector2(1, 0), Vector2(0, 1)]
 		var Calculations = fmod(dir + 180 + 45, 360)/90
-
-
+		
+		
 		var ChanceOfAdjusment = int(Calculations) - Calculations
 		var Adjustment = 0
-
-		if abs((ChanceOfAdjusment + 0.5)) * 100 >= Rng.randf_range(0, 100):
+		
+		
+		if is_on_floor():
 			if ChanceOfAdjusment + 0.5 <= 0:
 				Adjustment = -1
 			if ChanceOfAdjusment + 0.5 >= 0:
 				Adjustment = 1
-
+		
+		elif abs((ChanceOfAdjusment + 0.5)) * 100 >= Rng.randf_range(0, 100):
+			if ChanceOfAdjusment + 0.5 <= 0:
+				Adjustment = -1
+			if ChanceOfAdjusment + 0.5 >= 0:
+				Adjustment = 1
+		
 		var Usebal_dir = Directions[(int(Calculations) - Adjustment) % 4]
-
-		move_and_slide(Usebal_dir * 8 / delta)
+		
+		move_and_slide(Usebal_dir * 8 / delta, UP)
 		Move = false
 
 
