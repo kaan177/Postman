c72fe8113ffe34baa5a8b84494b2ae6428c90902
kaan177 <66557991+kaan177@users.noreply.github.com>
2022-11-14 17:26:14 +0100

    implemented new code for jumping

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,9 +9,11 @@ const UP = Vector2(0, -1)
 #Player stats
 export var Max_speed = 100
 export var Accelaration = 10
-export var Jump_height = 350
-export var Gravity = UP * 10
 export var Friction = 0.2
+var Jump_height = 100
+var Jump_time_to_peak = 0.5
+var Jump_time_to_descent = 0.4
+var Gravity = UP * 10
 
 #Remaining variables
 var Motion = Vector2.ZERO
@@ -20,11 +22,14 @@ var Lettervector = Vector2.RIGHT
 #Loading variables in
 onready var animation_player = $AnimationPlayer
 onready var sprite = $Sprite
+onready var Jump_velocity = ((2 * Jump_height) / Jump_time_to_peak) * -1
+onready var Jump_gravity = ((-2 * Jump_height) / (Jump_time_to_peak * Jump_time_to_peak)) * -1
+onready var fall_gravity = ((-2 * Jump_height) / (Jump_time_to_descent * Jump_time_to_descent)) * -1
 
 func _physics_process(delta):
 	#Handeling movement
 	if not Input.is_action_pressed("stand_still"):
-		Move()
+		Move(delta)
 	
 	if Input.is_action_just_pressed("letter_right") or Input.is_action_just_pressed("letter_left") or Input.is_action_just_pressed("letter_up") or Input.is_action_just_pressed("letter_down"):
 		LetterThrow()
@@ -33,10 +38,12 @@ func _physics_process(delta):
 
 
 
-
-func Move():
+func Move(delta):
 	var frictiondetection = false
-	Motion -= Gravity
+	if Motion.y > 0:
+		Motion.y += Jump_gravity * delta
+	else:
+		Motion.y += fall_gravity * delta
 	#Turning user input into movement
 	if Input.is_action_pressed("right"):
 		Motion.x = min(Motion.x + Accelaration, Max_speed)
@@ -56,14 +63,10 @@ func Move():
 		frictiondetection = true
 		if not Input.is_action_pressed("up") and is_on_floor():
 			animation_player.play("Default")
-	if Input.is_action_just_released("up"):
-		if Motion.y < -100:
-			get_node("Tween").interpolate_property(self,'Motion:y',Motion.y, -100, 0.15,Tween.TRANS_SINE,Tween.EASE_OUT)
-			get_node("Tween").start()
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("up"):
-			Motion.y = -Jump_height
+			Motion.y = Jump_velocity
 			animation_player.play("Jump")
 			
 		if frictiondetection == true:
