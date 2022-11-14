5795146833d77f9d0ef8be576cbf380150fbc99e
Moonblye <lars.van.dommelen@xs4all.nl>
2022-11-14 12:17:07 +0100

    Weg met print

--- Testing/TestWorld.gd
+++ Testing/TestWorld.gd
@@ -11,7 +11,6 @@ func _ready():
 
 #Spawnt de letter
 func Spawn_letter(Lettervector):
-	print(Lettervector)
 	if not $Letter in get_children():
 		var Letter = preload("res://Player/Letter.tscn").instance()
 		Letter.set_position(Player.position)
