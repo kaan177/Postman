00af4353021c334edc95a59d3f4664c116853735
kaan177 <66557991+kaan177@users.noreply.github.com>
2022-11-17 23:59:09 +0100

    water fixes

--- Objects/Water.gd
+++ Objects/Water.gd
@@ -1,13 +1,12 @@
 extends KinematicBody2D
 
 const GRAVITY = Vector2(0, 0.2)
-const UP = Vector2.UP
 
 var Vector = Vector2.ZERO
 var Rng = RandomNumberGenerator.new()
-var Move = false
 
 onready var world = get_parent()
+onready var ray_cast_2d = $RayCast2D
 
 
 #connenct signal
@@ -18,45 +17,44 @@ func _ready():
 	
 
 func make_a_move():
-	Move = true
+	Vector += GRAVITY
+	Vector = Vector.normalized()
+	var dir = rad2deg(Vector.angle())
+	
+	
+	var Directions = [Vector2(-1, 0), Vector2(0, -1), Vector2(1, 0), Vector2(0, 1)]
+	var Calculations = fmod(dir + 180 + 45, 360)/90
+	
+	
+	var ChanceOfAdjusment = int(Calculations) - Calculations
+	var Adjustment = 0
+	
+	
+	if ray_cast_2d.is_colliding():
+		if ChanceOfAdjusment + 0.5 <= 0:
+			Adjustment = -1
+		if ChanceOfAdjusment + 0.5 >= 0:
+			Adjustment = 1
+	
+	elif abs((ChanceOfAdjusment + 0.5)) * 100 >= Rng.randf_range(0, 100):
+		if ChanceOfAdjusment + 0.5 <= 0:
+			Adjustment = -1
+		if ChanceOfAdjusment + 0.5 >= 0:
+			Adjustment = 1
+	
+	var Usebal_dir = Directions[(int(Calculations) - Adjustment) % 4]
+	
+	print(Usebal_dir)
+	
+	move_and_collide(Usebal_dir)
+	
+	print(position)
+	
+	position = Vector2(int(round(position.x)), int(round(position.y)))
+	print(position)
 
 
-#the movement code
 
-
-func _physics_process(delta):
-	if Move:
-		Vector += GRAVITY
-		Vector = Vector.normalized()
-		var dir = rad2deg(Vector.angle())
-		
-		
-		var Directions = [Vector2(-1, 0), Vector2(0, -1), Vector2(1, 0), Vector2(0, 1)]
-		var Calculations = fmod(dir + 180 + 45, 360)/90
-		
-		
-		var ChanceOfAdjusment = int(Calculations) - Calculations
-		var Adjustment = 0
-		
-		
-		if is_on_floor():
-			if ChanceOfAdjusment + 0.5 <= 0:
-				Adjustment = -1
-			if ChanceOfAdjusment + 0.5 >= 0:
-				Adjustment = 1
-		
-		elif abs((ChanceOfAdjusment + 0.5)) * 100 >= Rng.randf_range(0, 100):
-			if ChanceOfAdjusment + 0.5 <= 0:
-				Adjustment = -1
-			if ChanceOfAdjusment + 0.5 >= 0:
-				Adjustment = 1
-		
-		var Usebal_dir = Directions[(int(Calculations) - Adjustment) % 4]
-		
-		move_and_slide(Usebal_dir / delta, UP)
-		position = Vector2(int(position.x), int(position.y))
-		Move = false
-		print(position)
 
 
 
