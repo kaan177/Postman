9e92a20a68d59f63aa6d2156845de07be9e61acc
kaan177 <66557991+kaan177@users.noreply.github.com>
2022-11-24 17:52:59 +0100

    made the platform and button signal

--- Scripts/ApearingPlatform.gd
+++ Scripts/ApearingPlatform.gd
@@ -4,16 +4,24 @@ onready var sprite = $Sprite
 onready var collisionshape = $CollisionShape2D
 onready var timer = $Timer
 
+export var Time = 1
 
+var TimerIsPlaying = false
+
+func act():
+	Appear()
 
 func Appear():
 	sprite.visible = true
-	collisionshape.disabled = false
+	collisionshape.set_deferred("disabled", false)
+	timer.start(Time)
+	TimerIsPlaying = true
 
 func Disappear():
 	sprite.visible = false
-	collisionshape.disabled = true
+	collisionshape.set_deferred("disabled", true)
 
 
 func _on_Timer_timeout():
 	Disappear()
+	TimerIsPlaying = false
--- Scripts/Buttons.gd
+++ Scripts/Buttons.gd
@@ -1,15 +1,19 @@
 extends Area2D
 
+signal ButtonPressed()
+
 export var Conected_node :NodePath
+
 onready var sprite = $Sprite
 
 var BodyAmount = 0
 
 func _ready():
 	yield(get_tree(),"idle_frame")
-	
+	connect("ButtonPressed", get_node(Conected_node), "act")
 
 func _on_Buttons_body_entered(body):
+	emit_signal("ButtonPressed")
 	BodyAmount += 1
 	if BodyAmount > 0:
 		sprite.frame = 1
